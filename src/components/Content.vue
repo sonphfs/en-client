f38b49f9a7d86cd835dbb5cbf8c9e47de096a152
sonphfs <sonph@kaopiz.com>
2019-10-04 17:48:48 +0700

    update layout

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,72 +1,198 @@
 <template>
-    <div>
-        <!-- partial -->
-      <div class="container-fluid page-body-wrapper">
-        <div class="main-panel" style="width: 100%;">
-          <div class="content-wrapper">
-            <!-- Page Title Header Starts-->
-            <div class="row page-title-header">
-              <div class="col-12">
-                <div class="page-header">
-                  <h4 class="page-title">Dashboard</h4>
-                  <div class="quick-link-wrapper w-100 d-md-flex flex-md-wrap">
-                    <ul class="quick-links">
-                      <li><a href="#">ICE Market data</a></li>
-                      <li><a href="#">Own analysis</a></li>
-                      <li><a href="#">Historic market data</a></li>
-                    </ul>
-                  </div>
-                </div>
+  <div>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item nav-profile">
+            <a href="#" class="nav-link">
+              <div class="profile-image">
+                <img
+                  class="img-xs rounded-circle"
+                  src="../assets/images/faces/face8.jpg"
+                  alt="profile image"
+                />
+                <div class="dot-indicator bg-success"></div>
               </div>
-              <div class="col-md-12">
-                <div class="page-header-toolbar">
-                  // TODO
+              <div class="text-wrapper">
+                <p class="profile-name">Allen Moreno</p>
+                <p class="designation">Premium user</p>
+              </div>
+            </a>
+          </li>
+          <li class="nav-item nav-category">Main Menu</li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">
+              <i class="menu-icon typcn typcn-document-text"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              data-toggle="collapse"
+              href="#ui-basic"
+              aria-expanded="false"
+              aria-controls="ui-basic"
+            >
+              <i class="menu-icon typcn typcn-coffee"></i>
+              <span class="menu-title">Basic UI Elements</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/ui-features/typography.html">Typography</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pages/forms/basic_elements.html">
+              <i class="menu-icon typcn typcn-shopping-bag"></i>
+              <span class="menu-title">Form elements</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pages/charts/chartjs.html">
+              <i class="menu-icon typcn typcn-th-large-outline"></i>
+              <span class="menu-title">Charts</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pages/tables/basic-table.html">
+              <i class="menu-icon typcn typcn-bell"></i>
+              <span class="menu-title">Tables</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pages/icons/font-awesome.html">
+              <i class="menu-icon typcn typcn-user-outline"></i>
+              <span class="menu-title">Icons</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              data-toggle="collapse"
+              href="#auth"
+              aria-expanded="false"
+              aria-controls="auth"
+            >
+              <i class="menu-icon typcn typcn-document-add"></i>
+              <span class="menu-title">User Pages</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/samples/blank-page.html">Blank Page</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/samples/login.html">Login</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/samples/register.html">Register</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/samples/error-404.html">404</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/samples/error-500.html">500</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <!-- Page Title Header Starts-->
+          <div class="row page-title-header">
+            <div class="col-12">
+              <div class="page-header">
+                <h4 class="page-title">Dashboard</h4>
+                <div class="quick-link-wrapper w-100 d-md-flex flex-md-wrap">
+                  <ul class="quick-links">
+                    <li>
+                      <a href="#">ICE Market data</a>
+                    </li>
+                    <li>
+                      <a href="#">Own analysis</a>
+                    </li>
+                    <li>
+                      <a href="#">Historic market data</a>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
-            <!-- Page Title Header Ends-->
-            <div class="row">
-              <div class="col-md-12 grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="row">
-                    // TODO
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <Part2Component></Part2Component>
-              <SidebarComponent></SidebarComponent>
-            </div>
-            <div class="row">
+            <div class="col-md-12">
+              <div class="page-header-toolbar">// TODO</div>
             </div>
           </div>
-          <!-- content-wrapper ends -->
-          <!-- partial:partials/_footer.html -->
-          <footer class="footer">
-            <div class="container-fluid clearfix">
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2019</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">// TODO<i class="mdi mdi-heart text-danger"></i>
-              </span>
+          <!-- Page Title Header Ends-->
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">// TODO</div>
+                </div>
+              </div>
             </div>
-          </footer>
-          <!-- partial -->
+          </div>
+          <div class="row">
+            <Part2Component></Part2Component>
+            <SidebarComponent></SidebarComponent>
+          </div>
+          <div class="row"></div>
         </div>
-        <!-- main-panel ends -->
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="container-fluid clearfix">
+            <span
+              class="text-muted d-block text-center text-sm-left d-sm-inline-block"
+            >Copyright © 2019</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">
+              // TODO
+              <i class="mdi mdi-heart text-danger"></i>
+            </span>
+          </div>
+        </footer>
+        <!-- partial -->
       </div>
-      <!-- page-body-wrapper ends -->
+      <!-- main-panel ends -->
     </div>
+    <!-- page-body-wrapper ends -->
+  </div>
 </template>
 
 <script>
-import Part2Component from './examinations/Part2Component.vue'
-import SidebarComponent from './examinations/SidebarComponent.vue'
+import Part2Component from "./examinations/Part2Component.vue";
+import SidebarComponent from "./examinations/SidebarComponent.vue";
 export default {
-    name : "Content",
-    components: {
-      Part2Component, SidebarComponent
-    }
-}
+  name: "Content",
+  components: {
+    Part2Component,
+    SidebarComponent
+  }
+};
 </script>
+
+<style>
+  .container-fluid.page-body-wrapper {
+    padding-top: 63px;
+  }
+  .container-fluid {
+    padding-right: 0px;
+    padding-left: 0px;
+  }
+</style>
