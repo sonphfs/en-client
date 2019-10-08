eff0d107859d1b8b3deef3f9be1721f9f1c1fd8c
sonphfs <sonph@kaopiz.com>
2019-10-08 17:37:25 +0700

    install vuex, axios

--- src/components/landing_pages/layouts/SignupForm.vue
+++ src/components/landing_pages/layouts/SignupForm.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="col-lg-5 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="500">
     <form
-      action="http://127.0.0.1:5500/templates/Oneschool/index.html"
-      method="post"
-      class="form-box"
+            @submit.prevent="register"
+            class="form-box"
     >
       <h3 class="h4 text-black mb-4">Sign Up</h3>
       <h3 class="h4 text-black mb-4"> {{ token }}</h3>
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Use Name" />
+        <input type="text" v-bind="username" class="form-control" placeholder="Use Name" />
       </div>
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Email Addresss" />
+        <input type="text" v-bind="email" class="form-control" placeholder="Email Addresss" />
       </div>
       <div class="form-group">
-        <input type="password" class="form-control" placeholder="Password" />
+        <input type="password" v-bind="password" class="form-control" placeholder="Password" />
       </div>
       <div class="form-group mb-4">
-        <input type="password" class="form-control" placeholder="Re-type Password" />
+        <input type="password" v-bind="password_confirmation" class="form-control" placeholder="Re-type Password" />
       </div>
       <div class="form-group">
         <input type="submit" class="btn btn-primary btn-pill" value="Sign up" />
