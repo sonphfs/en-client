5a530750858e8ad9f29048e6a2b3302ce8920220
sonphfs <sonph@kaopiz.com>
2019-10-08 17:37:25 +0700

    vuex

--- src/components/landing_pages/layouts/SignupForm.vue
+++ src/components/landing_pages/layouts/SignupForm.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="col-lg-5 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="500">
     <form
-      action="http://127.0.0.1:5500/templates/Oneschool/index.html"
-      method="post"
-      class="form-box"
+            @submit.prevent="register"
+            class="form-box"
     >
       <h3 class="h4 text-black mb-4">Sign Up</h3>
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Use Name" />
+        <input type="text" v-bind="username" class="form-control" placeholder="Use Name" />
       </div>
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Email Addresss" />
+        <input type="text" v-bind="email" class="form-control" placeholder="Email Addresss" />
       </div>
       <div class="form-group">
-        <input type="password" class="form-control" placeholder="Password" />
+        <input type="password" v-bind="password" class="form-control" placeholder="Password" />
       </div>
       <div class="form-group mb-4">
-        <input type="password" class="form-control" placeholder="Re-type Password" />
+        <input type="password" v-bind="password_confirmation" class="form-control" placeholder="Re-type Password" />
       </div>
       <div class="form-group">
         <input type="submit" class="btn btn-primary btn-pill" value="Sign up" />
@@ -30,7 +29,23 @@ export default {
   name: "SignupForm",
   data(){
     return {
-      
+      email : '',
+      username: '',
+      password: '',
+      password_confirmation: '',
+    }
+  },
+  methods: {
+    register() {
+        let data = {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+          password_confirmation: this.password_confirmation
+        }
+        this.$store.dispatch('register', data)
+            .then(()=> this.$router.push('/'))
+            .catch(err => console.log(err))
     }
   }
 }
