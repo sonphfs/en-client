fd0cd872ce4d9e0c5751a2474e45bcac4ae7d6b2
sonphfs <sonph@kaopiz.com>
2019-12-05 18:45:21 +0700

    update test word

---
+++ src/components/elements/Question.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-body title-question">
+          <p class="question-content">{{ question.content }}</p>
+          <p class="pronun">/färmər/</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12 answer-body">
+      <div class="card">
+        <div class="card-body" style="text-align: center">
+          <div class="answer">
+            <div class="answer-item" @click="choose('A', $event)">{{question.answer_A}}</div>
+            <div class="answer-item" @click="choose('B', $event)">{{question.answer_B}}</div>
+            <div class="answer-item" @click="choose('C', $event)">{{question.answer_C}}</div>
+            <div class="answer-item" @click="choose('D', $event)">{{question.answer_D}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Question",
+  props: ["item"],
+  data() {
+    return {
+      question: {
+        content: "What is Your name?",
+        answer_A: "My name is Hele",
+        answer_B: "Fuking",
+        answer_C: "Yolo",
+        answer_D: "Dixa",
+        correct_answer: "A"
+      },
+      isChoose: false,
+      isCorrect: false
+    };
+  },
+  created() {
+    if (this.item != undefined) {
+      this.question = this.item;
+    }
+  },
+  methods: {
+    choose(answer) {
+      if (answer != this.question.correct_answer) {
+        console.log(
+          event.target.setAttribute("class", "answer-item btn-danger")
+        );
+      } else {
+        console.log(
+          event.target.setAttribute("class", "answer-item btn-success")
+        );
+      }
+      let that = this
+      setTimeout(function() {
+        that.nextQuestion();
+      }, 1000);
+    },
+    nextQuestion() {
+      this.$emit("nextQuestion");
+    }
+  }
+};
+</script>
+<style scoped>
+p.pronun {
+  color: aqua;
+}
+div.title-question {
+  text-align: center;
+}
+div.answer-body .card .card-body {
+  text-align: center;
+  width: 50%;
+  margin-left: 25%;
+  margin-right: 25%;
+}
+div.button-next {
+  margin-top: 10px;
+  padding-left: 5%;
+  padding-right: 5%;
+  text-align: right;
+}
+div.answer {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+}
+div.answer-body {
+  margin-top: 10px;
+}
+div.answer-item {
+  padding: 10px;
+  border: solid 1px #dde4eb;
+  border-radius: 25px;
+}
+div.answer-item:hover {
+  background-color: #dde4eb;
+}
+</style>
--- src/components/pages/Test.vue
+++ src/components/pages/Test.vue
@@ -1,25 +1,6 @@
 <template>
   <div>
-    <div class="col-md-12">
-      <div class="card">
-        <div class="card-body title-question">
-          <p class="question-content">Chọn từ đúng phiên âm sau</p>
-          <p class="pronun">/färmər/</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-12 answer-body">
-      <div class="card">
-        <div class="card-body" style="text-align: center">
-          <div class="answer">
-            <div class="answer-item">comedy</div>
-            <div class="answer-item">carpenter</div>
-            <div class="answer-item">barber</div>
-            <div class="answer-item">Đáp án khác</div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Question v-for="(question, key) in exam.questions" :item="question" v-show="key++==step" @nextQuestion="nextStep"></Question>
     <div class="col-md-12 progress-test">
       <div class="progress">
         <div
@@ -32,45 +13,39 @@
         ></div>
       </div>
     </div>
-    <div class="col-md-12 button-next">
-      <button type="button" class="btn btn-primary btn-rounded btn-fw">Next</button>
-    </div>
   </div>
 </template>
 
-<style scoped>
-p.pronun {
-  color: aqua;
-}
-div.title-question {
-  text-align: center;
-}
-div.answer-body .card .card-body {
-  text-align: center;
-  width: 50%;
-  margin-left: 25%;
-  margin-right: 25%;
-}
-div.button-next {
-  margin-top: 10px;
-  padding-left: 5%;
-  padding-right: 5%;
-  text-align: right;
-}
-div.answer {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 10px;
-}
-div.answer-body {
-  margin-top: 10px;
-}
-div.answer-item {
-  padding: 10px;
-  border: solid 1px #dde4eb;
-  border-radius: 25px;
-}
-div.answer-item:hover {
-    background-color: #dde4eb;
-}
-</style>
+<script>
+import Question from "@/components/elements/Question";
+import request from "@/utils/request";
+export default {
+  name: "Test",
+  components: {
+    Question
+  },
+  data(){
+    return {
+        exam: {},
+        step: 1
+    }
+  },
+  created(){
+      this.getExam()
+  },
+  methods: {
+    getExam(){
+        request({
+          url: "/test/1"
+        }).then(res=> {
+            this.exam = res.data.result_data
+        }).catch(err=>{
+
+        })
+    },
+    nextStep(){
+      this.step++
+    }
+  },
+};
+</script>
